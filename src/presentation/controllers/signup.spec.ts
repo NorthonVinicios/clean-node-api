cb17afe04bd050f0bfa5a20e85f8b1751d54c6dc
northon <northon.utfpr@gmail.com>
2022-01-19 08:42:08 -0300

    test: ensure signUpController return status 400 if no email is provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,7 @@ describe('SignUp Controllers', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        mail: 'any_mail@mail.com',
+        email: 'any_mail@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
 
@@ -13,5 +13,20 @@ describe('SignUp Controllers', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: name'))
+  })
+  test('Should return 400 if no email is provide', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
   })
 })
